d4532df98872ce1613fe85f0eb8fc8b0a3fda187
12301013-SunHaobo <12301013@bjtu.edu.cn>
2015-03-19 20:32:22 +0800

    12301013-SunHaobo

--- app/src/main/java/com/deitel/twittersearches/MainActivity.java
+++ app/src/main/java/com/deitel/twittersearches/MainActivity.java
@@ -134,8 +134,7 @@ public class MainActivity extends ListActivity
       {
          // get query string and create a URL representing the search
          String tag = ((TextView) view).getText().toString();
-         String urlString = getString(R.string.searchURL) +
-            Uri.encode(savedSearches.getString(tag, ""), "UTF-8");
+          String urlString ="http://" +Uri.encode(savedSearches.getString(tag, ""), "UTF-8");
          
          // create an Intent to launch a web browser    
          Intent webIntent = new Intent(Intent.ACTION_VIEW, 
